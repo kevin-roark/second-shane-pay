9c6b88dea289f36024012623d7a735a62d2918da
Kevin Roark <ker2143@columbia.edu>
2015-09-26 01:41:15 -0400

    fleshed out the total payment side of things ... now need to grant the rewards

--- public/js/pay.js
+++ public/js/pay.js
@@ -1,28 +1,129 @@
 
 $(function() {
-  $.getJSON('/braintree_token', function(data) {
-    if (!data.token) {
-      console.log('fuck');
+
+  var $checkout = $('#checkout');
+
+  var $paymentPledgeInput = $('#payment-pledge-input');
+  var $paymentPledgeError = $('#payment-pledge-error');
+  var $makeThePledge = $('#make-the-pledge-button');
+  var $pledgeEnter = $('#pledge-enter');
+  var $thanksForPledging = $('#thanks-for-pledging');
+  var $pledgedAmount = $('#pledged-amount');
+  var $checkoutBlocker = $('#checkout-blocker');
+  var $paymentError = $('#payment-error');
+
+  var state = {
+    validPaymentAmount: false
+  };
+
+  setTimeout(function() {
+    $paymentPledgeInput.focus();
+  }, 1000);
+
+  function paymentAmountIsValid(valid) {
+    state.validPaymentAmount = valid;
+    if (valid) {
+      $paymentPledgeInput.removeClass('invalid');
+      $paymentPledgeInput.addClass('valid');
+      $makeThePledge.removeClass('disabled');
+      $makeThePledge.addClass('valid');
+    }
+    else {
+      $paymentPledgeInput.addClass('invalid');
+      $paymentPledgeInput.removeClass('valid');
+      $makeThePledge.addClass('disabled');
+      $makeThePledge.removeClass('valid');
+    }
+  }
+
+  $paymentPledgeInput.change(function() {
+    var value = Number($paymentPledgeInput.val());
+    if (isNaN(value)) {
+      paymentAmountIsValid(false);
+      $paymentPledgeError.text('Shane Requests Number Payments Only');
+    }
+    else {
+      var money = value.toFixed(2);
+      if (money < 2) {
+        paymentAmountIsValid(false);
+        $paymentPledgeError.text('Shane Requests at least $2.00 U.S. Dollars');
+      }
+      else {
+        paymentAmountIsValid(true);
+        $paymentPledgeError.text('');
+      }
+
+      $paymentPledgeInput.val(money);
+      //$paymentPledgeInput.attr('readonly', true);
+    }
+  });
+
+  $makeThePledge.click(function() {
+    if (!state.validPaymentAmount || state.pledgedPaymentAmount) {
       return;
     }
 
-    braintree.setup(data.token, "dropin", {
-      container: "payment-form",
-      onReady: function() {
-        console.log('ready to pay shane...');
-      },
-      onPaymentMethodReceived: function(paymentData) {
-        var postParameters = {
-          payment_method_nonce: paymentData.nonce
-        };
-        $.post('/checkout', postParameters, function(checkoutData) {
-          console.log(checkoutData);
-        });
-      },
-      onError: function(err) {
-        console.log(err);
+    var amount = Number($paymentPledgeInput.val()).toFixed(2);
+    state.pledgedPaymentAmount = amount;
+    $pledgedAmount.text('$' + amount + ' U.S. Dollars');
+
+    $pledgeEnter.fadeOut(500, function() {
+      $thanksForPledging.fadeIn(500);
+    });
+
+    $checkoutBlocker.fadeOut(800);
+    setupBraintree();
+  });
+
+  function setupBraintree() {
+    $.getJSON('/braintree_token', function(data) {
+      if (!data.token) {
+        console.log('fuck');
+        return;
+      }
+
+      braintree.setup(data.token, "dropin", {
+        container: "payment-form",
+        onReady: readyToPay,
+        onPaymentMethodReceived: paymentMethodRecieved,
+        onError: paymentMethodError
+      });
+    });
+  }
+
+  function readyToPay() {}
+
+  function paymentMethodError(err) {
+    console.log('payment error...');
+    console.log(err);
+  }
+
+  function paymentMethodRecieved(paymentData) {
+    if (state.hasSubmittedPayment) {
+      return;
+    }
+
+    state.hasSubmittedPayment = true;
+    $paymentError.text('');
+
+    var postParameters = {
+      payment_method_nonce: paymentData.nonce
+    };
+    $.post('/checkout', postParameters, function(checkoutData) {
+      if (!checkoutData.success) {
+        console.log('error making transaction:');
+        console.log(checkoutData.error);
+        $paymentError.text('Transaction Error ... Please Submit Again.');
+        state.hasSubmittedPayment = false;
+      }
+      else {
+        grantRewardsWithAmount(checkoutData.amount);
       }
     });
-  });
+  }
+
+  function grantRewardsWithAmount(amount) {
+
+  }
 
 });
